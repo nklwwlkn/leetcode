5750bc631537e93f99f679cee70ea5c5e26a98fe
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2022-08-24 08:53:13 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- solutions/problems/contains_duplicate/solution.js
+++ solutions/problems/contains_duplicate/solution.js
@@ -3,15 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let obj = {};
-
-    for (let val of nums) {
-        obj[val] = (obj[val] || 0) + 1;
-    }
-
-    for (let val in obj) {
-        if (obj[val] > 1) return true 
-    }
-    
-    return false;
+    return [...new Set(nums)].length !== nums.length
 };

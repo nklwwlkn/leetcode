06006f20b21fae97d1962adb8698615ee884787e
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2022-11-06 12:03:04 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- solutions/problems/contains_duplicate/solution.js
+++ solutions/problems/contains_duplicate/solution.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return [...new Set(nums)].length !== nums.length
+   const m = new Map()
+   
+   for (let num of nums) {
+       if (m.get(num)) return true
+       
+       m.set(num, true)
+   }
+    
+    return false
 };

49e8f9e9c1b652511ae5e3fb6755b7b540bd89c9
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-16 21:53:43 +0000

    Sync LeetCode submission - Two Sum (python3)

--- solutions/problems/two_sum/solution.py
+++ solutions/problems/two_sum/solution.py
@@ -1,21 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         if len(nums) == 2:
-            return [0,1]
-        
-        hashmap = {}
-        
-        for idx, num in enumerate(nums):
-            hashmap[num] = idx
+            return [0, 1]
+
+        hmap = {}
+        nums_len = len(nums)
+
+        for i in range(nums_len):
+            hmap[nums[i]] = i
+
+        for i in range(nums_len):
+           find = target - nums[i]
+           if find in hmap and i != hmap.get(find):
+               return [i, hmap.get(find)]
             
 
-        for idx, num in enumerate(nums):
-            lookup = target - num
-            
-            if hashmap.get(lookup) and hashmap.get(lookup) != idx:
-                return [hashmap.get(lookup), idx]
-        
-        
-            
-            
         

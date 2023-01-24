e06355df20d288ebf355e1440778e578b088ea73
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-24 16:17:21 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- solutions/problems/top_k_frequent_elements/solution.py
+++ solutions/problems/top_k_frequent_elements/solution.py
@@ -1,30 +1,30 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums_len = len(nums)
-
-        if nums_len == k:
-            return nums
-        
-        bucket = [[] for i in range(nums_len)]
         hmap = {}
 
         for num in nums:
             hmap[num] = hmap.get(num, -1) + 1
+        
+        bucket = [[] for _ in range(len(nums))]
 
         for key in hmap:
-            bucket[hmap[key]].append(key)
-        
-        res = []
+            bucket[hmap.get(key)].append(key)
 
+        mostFreq = []
         print(bucket)
+        for i in range(len(bucket) -1, -1, -1):
+            for num in bucket[i]:
+                mostFreq.append(num)
+            if len(mostFreq) == k:
+                break
         
-        i = len(bucket) - 1
-        while len(res) != k:
-            for elem in bucket[i]:
-                res.append(elem)
-            i -= 1
+        return mostFreq
+    
 
-        return res
 
-            
+
+
+       
+
+
         

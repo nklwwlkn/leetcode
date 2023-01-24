f98585a791aae637cf833bafc6c0a3b8359a3e5f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-24 19:52:02 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- solutions/problems/valid_palindrome/solution.py
+++ solutions/problems/valid_palindrome/solution.py
@@ -1,35 +1,24 @@
 class Solution:
-    def sanitize(self, s: str) -> str:
-        res = ''
-        for char in s:
-            ord_char = ord(char)
-            if ord_char >= 65 and ord_char <= 90:
-                res += char.lower()
-            elif ord_char >= 97 and ord_char <= 122:
-                res += char
-            elif ord_char >= 48 and ord_char <= 57:
-                res += char
-        
-        return res
-                
-    
     def isPalindrome(self, s: str) -> bool:
-        sanitized_s = self.sanitize(s)
-        
-        if len(sanitized_s) == 0:
-            return True
-        
-        if len(sanitized_s) == 1:
-            return True
-        
-        i = 0
-        k = (len(sanitized_s) - 1)
-        
-        while i < k:
-            if sanitized_s[i] != sanitized_s[k]:
+        start = 0
+        end = len(s) - 1
+
+
+        while start < end:
+            val_left = s[start].lower()
+            val_right = s[end].lower()
+    
+            if not val_left.isalnum():
+                start += 1
+                continue
+            if not val_right.isalnum():
+                end -= 1
+                continue
+
+            if val_left != val_right:
                 return False
-            i += 1
-            k -= 1
-            
+
+            start += 1
+            end -= 1
+
         return True
-        

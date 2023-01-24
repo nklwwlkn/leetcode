3962a235d32794da616771f8fc01fa36a3f8b8bc
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-24 16:32:19 +0000

    Sync LeetCode submission - Valid Sudoku (python3)

---
+++ solutions/problems/valid_sudoku/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        sqrs = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                value = board[r][c]
+
+                if value == ".":
+                    continue
+
+                if value in rows[r] or value in cols[c] or value in sqrs[(r // 3, c // 3)]:
+                    return False
+
+                rows[r].add(value)
+                cols[c].add(value)
+                sqrs[(r // 3, c // 3)].add(value)
+        
+        return True
+        

84a9772aa3b4c840b750a8490557c2ed6bdfa46b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-25 11:57:49 +0000

    Sync LeetCode submission - 3Sum (python3)

--- solutions/problems/3sum/solution.py
+++ solutions/problems/3sum/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        z = []
         p = []
         n = []
-        z = []
-        res = set()
-              
+
         for num in nums:
             if num > 0:
                 p.append(num)
@@ -12,35 +12,35 @@ class Solution:
                 n.append(num)
             else:
                 z.append(num)
-        
-        P = set(p)
-        N = set(n)
-        
+            
+        posSet = set(p)
+        negSet = set(n)
+
         if z:
-            for num in P:
-                if -num in N:
-                    res.add(tuple(sorted([-num, 0, num])))
+            for num in posSet:
+                if -num in negSet:
+                    res.add(tuple(sorted([num, -num, 0])))
         
         if len(z) >= 3:
             res.add(tuple([0,0,0]))
-            
-        
+
         for i in range(len(p)):
             for j in range(i + 1, len(p)):
-                target = -1 * (p[i] + p[j])
+                val_i = p[i]
+                val_j = p[j]
+                target = -1 * (val_i + val_j)
                 
-                if target in N:
-                    res.add(tuple(sorted([target, p[i], p[j]])))
+                if target in negSet:
+                    res.add(tuple(sorted([target, val_i, val_j])))
         
         for i in range(len(n)):
             for j in range(i + 1, len(n)):
-                target = -1 * (n[i] + n[j])
+                val_i = n[i]
+                val_j = n[j]
+                target = -1 * (val_i + val_j)
                 
-                if target in P:
-                    res.add(tuple(sorted([target, n[i], n[j]])))
-        
+                if target in posSet:
+                    res.add(tuple(sorted([target, val_i, val_j])))
         
         return res
             
-            
-        

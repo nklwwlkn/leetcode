ffd8ef7b95bc92826cb7664f6ca5465a0f5ddd19
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-01-29 16:21:09 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- solutions/problems/valid_anagram/solution.py
+++ solutions/problems/valid_anagram/solution.py
@@ -2,21 +2,21 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
+
         hmap_s = {}
         hmap_t = {}
-    
-        for i in range(len(s)):
-            s_char = s[i]
-            t_char = t[i]
 
-            hmap_s[s_char] = hmap_s.get(s_char, 0) + 1
-            hmap_t[t_char] = hmap_t.get(t_char, 0) + 1
-        
+        for i in range(len(s)):
+            val_s = s[i]
+            val_t = t[i]
+            hmap_s[val_s] = hmap_s.get(val_s, 0) + 1
+            hmap_t[val_t] = hmap_t.get(val_t, 0) + 1
+
         for key in hmap_s:
-            if key not in hmap_t or hmap_s.get(key, 0) != hmap_t.get(key, 1):
+            if key not in hmap_t or hmap_s[key] != hmap_t[key]:
                 return False
         
         return True
+
 
         

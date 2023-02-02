71a5a7a039533c8e985cb21d1a92676ea663633d
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-02 14:09:42 +0000

    Sync LeetCode submission - Fizz Buzz (python3)

---
+++ solutions/problems/fizz_buzz/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for num in range(1, n + 1):
+            print(num)
+            if num % 3 == 0 and num % 5 == 0:
+                res.append("FizzBuzz")
+            elif num % 3 == 0:
+                res.append("Fizz")
+            elif num % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(num))
+        return res

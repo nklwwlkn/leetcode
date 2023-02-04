06f70c82de3f164825009741dcd9beafefe9d26f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-04 23:00:53 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (python3)

--- solutions/problems/longest_consecutive_sequence/solution.py
+++ solutions/problems/longest_consecutive_sequence/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        counter = 0
-
+        maxCounter = 0
         hset = set()
+
         for num in nums:
             hset.add(num)
-
+        
         for num in nums:
             if num - 1 not in hset:
-                length = 1
-                while num + length in hset:
-                    length += 1
-                counter = max(counter, length)
-
-        return counter
+                counter = 1
+                while num + counter in hset:
+                    counter += 1
+                maxCounter = max(maxCounter, counter)
         
+        return maxCounter

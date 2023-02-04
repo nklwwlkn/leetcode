7059d27dbddd1170fdbfb3eb62fab3d1d72174ac
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-04 21:05:20 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- solutions/problems/product_of_array_except_self/solution.py
+++ solutions/problems/product_of_array_except_self/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        ans = [1] * size
-
+        res = [1] * len(nums)
         prefix = 1
-        for i in range(size):
-            ans[i] *= prefix
+        for i in range(len(nums)):
+            res[i] *= prefix
             prefix *= nums[i]
 
         postfix = 1
-        for i in range(size -1, -1, -1):
-            ans[i] *= postfix
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
             postfix *= nums[i]
         
-        return ans
+        return res
+
+        

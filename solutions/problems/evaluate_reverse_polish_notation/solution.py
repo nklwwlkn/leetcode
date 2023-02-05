0ddfbe49decd634642dca34090f00d561b9464c3
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-05 00:34:57 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- solutions/problems/evaluate_reverse_polish_notation/solution.py
+++ solutions/problems/evaluate_reverse_polish_notation/solution.py
@@ -7,13 +7,15 @@ class Solution:
             elif token == "-":
                 a, b = stack.pop(), stack.pop()
                 stack.append(b - a)
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
             elif token == "/":
                 a, b = stack.pop(), stack.pop()
                 stack.append(int(b / a))
-            elif token == "*":
-                stack.append(stack.pop() * stack.pop())
             else:
                 stack.append(int(token))
-        
+
         return stack[-1]
+                
+                
         

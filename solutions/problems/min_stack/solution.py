60308535381d692edd8ca2f12a503b4a40052e42
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-05 00:18:05 +0000

    Sync LeetCode submission - Min Stack (python3)

--- solutions/problems/min_stack/solution.py
+++ solutions/problems/min_stack/solution.py
@@ -3,12 +3,14 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.minStack = []
+        
 
     def push(self, val: int) -> None:
-        minVal = min(self.minStack[-1] if self.minStack else val, val)
-
         self.stack.append(val)
-        self.minStack.append(minVal)
+       
+        val = min(self.minStack[-1], val) if self.minStack else val 
+
+        self.minStack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()

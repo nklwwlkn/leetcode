7ca16614502b581551be44de68a1245fe95e0018
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-05 13:26:36 +0000

    Sync LeetCode submission - Car Fleet (python3)

--- solutions/problems/car_fleet/solution.py
+++ solutions/problems/car_fleet/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pairs = [(position[i], speed[i]) for i in range(len(position))]
+        fleetCounter = 0
         currentTime = 0
-        fleetCount = 0
 
         for pos, spd in sorted(pairs, reverse=True):
             destinationTime = (target - pos) / spd
-
             if destinationTime > currentTime:
-                currentTime = destinationTime
-                fleetCount += 1
-            
-        return fleetCount
+                fleetCounter += 1
+                currentTime = max(currentTime, destinationTime)
+
         
+        return fleetCounter
+

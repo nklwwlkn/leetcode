ea37d4cc5f61cb23f7bd288073adf6061bc052ee
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-08 18:29:19 +0000

    Sync LeetCode submission - Daily Temperatures (python3)

--- solutions/problems/daily_temperatures/solution.py
+++ solutions/problems/daily_temperatures/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
+        temperaturesSize = len(temperatures)
+        res = [0] * temperaturesSize
         stack = []
 
-        for i, t in enumerate(temperatures):
-            while stack and stack[-1][1] < t:
-                iStack, tStack = stack.pop()
+        for i in range(temperaturesSize):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                iStack = stack.pop()
                 res[iStack] = i - iStack
-            stack.append([i, t])
+            stack.append(i)
         
         return res
         

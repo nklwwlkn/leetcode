1b59fba872a851b7b161105f6b5f96e44b9b93c6
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-09 12:30:58 +0000

    Sync LeetCode submission - Binary Search (python3)

--- solutions/problems/binary_search/solution.py
+++ solutions/problems/binary_search/solution.py
@@ -3,13 +3,15 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = l + (r - l // 2)
-
-            if target < nums[mid]:
-                r = mid - 1
-            elif target > nums[mid]:
-                l = mid + 1
+            m = (r - l) // 2 + l
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
             else:
-                return mid
-            
+                return m
+        
         return -1
+        
+
+        

4cf8a44f242c2ce6aeeff2c2678e14768e2bdd91
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-10 22:40:23 +0000

    Sync LeetCode submission - Time Based Key-Value Store (python3)

---
+++ solutions/problems/time_based_key-value_store/solution.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.s = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.s[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        l, r = 0, len(self.s[key]) - 1
+        value = ""
+
+        while l <= r:
+            m = (r - l) // 2 + l
+
+            if  timestamp >= self.s[key][m][1]:
+                l = m + 1
+                value = self.s[key][m][0]
+            else:
+                r = m - 1
+        
+        return value
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)

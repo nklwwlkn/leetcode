8a365b3fdace1f6b94d5b952eba4c1f2ebacc109
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-02-18 19:25:59 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (python3)

--- solutions/problems/median_of_two_sorted_arrays/solution.py
+++ solutions/problems/median_of_two_sorted_arrays/solution.py
@@ -6,22 +6,23 @@ class Solution:
 
         if len(B) < len(A):
             A, B = B, A
-        
+
         l, r = 0, len(A) - 1
+
         while True:
             midA = (r - l) // 2 + l
             midB = half - midA - 2
 
-            aLeft = A[midA] if midA >= 0 else float("-inf")
-            aRight = A[midA + 1] if midA + 1 < len(A) else float("inf")
-            bLeft = B[midB] if midB >= 0 else float("-inf")
-            bRight = B[midB + 1] if midB + 1 < len(B) else float("inf")
+            aLeft = A[midA] if midA >= 0 else float('-inf')
+            aRight = A[midA + 1] if midA + 1 < len(A) else float('inf')
+            bLeft = B[midB] if midB >= 0 else float('-inf')
+            bRight = B[midB + 1] if midB + 1 < len(B) else float('inf')
 
             if aLeft <= bRight and bLeft <= aRight:
                 if total % 2:
                     return min(aRight, bRight)
                 else:
-                    return (max(aLeft, bLeft) + min(bRight, aRight)) / 2
+                    return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
             elif aLeft > bRight:
                 r = midA - 1
             else:

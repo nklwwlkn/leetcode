2a6cb21132342ff8692057e82074b3b568189094
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-01 19:51:22 +0000

    Sync LeetCode submission - 3Sum (python3)

--- solutions/problems/3sum/solution.py
+++ solutions/problems/3sum/solution.py
@@ -6,39 +6,34 @@ class Solution:
         res = set()
 
         for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0:
-                n.append(num)
-            else:
+            if num == 0:
                 z.append(num)
+            elif num > 0:
+                p.append(num)
+            else:
+                n.append(num)
         
+        P = set(p)
+        N = set(n)
 
         if len(z) >= 3:
-            res.add(tuple([0,0,0]))
-
-        N = set(n)
-        P = set(p)
-
-        if z:
-            for num in p:
+            res.add((0,0,0))
+        
+        if len(z) > 0:
+            for num in P:
                 if -num in N:
-                    res.add(tuple(sorted([-num, 0, num])))
+                    res.add((-num, 0, num))
 
         for i in range(len(p)):
             for j in range(i + 1, len(p)):
                 target = -1 * (p[i] + p[j])
                 if target in N:
                     res.add(tuple(sorted([target, p[i], p[j]])))
-
+        
         for i in range(len(n)):
             for j in range(i + 1, len(n)):
                 target = -1 * (n[i] + n[j])
                 if target in P:
                     res.add(tuple(sorted([target, n[i], n[j]])))
-        
+
         return res
-        
-
-
-        

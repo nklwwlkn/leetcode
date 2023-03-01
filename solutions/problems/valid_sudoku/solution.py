9901a7839c26f624a6bff9c71f93b9c6e2bb599e
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-01 16:51:31 +0000

    Sync LeetCode submission - Valid Sudoku (python3)

--- solutions/problems/valid_sudoku/solution.py
+++ solutions/problems/valid_sudoku/solution.py
@@ -4,17 +4,16 @@ class Solution:
         cols = defaultdict(set)
         sqrs = defaultdict(set)
 
-        for row in range(9):
-            for col in range(9):
-                value = board[row][col]
+        for r in range(9):
+            for c in range(9):
+                value = board[r][c]
                 if value == ".":
                     continue
-                if value in rows[row] or value in cols[col] or value in sqrs[(row // 3, col // 3)]:
+                if value in rows[r] or value in cols[c] or value in sqrs[(r // 3, c // 3)]:
                     return False
-                
-                rows[row].add(value)
-                cols[col].add(value)
-                sqrs[(row // 3, col // 3)].add(value)
+                rows[r].add(value)
+                cols[c].add(value)
+                sqrs[(r // 3, c //3)].add(value)
+        return True
                 
         
-        return True

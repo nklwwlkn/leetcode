3097963341715f1f055cf272775e9959bcc328ed
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-02 19:41:26 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (python3)

--- solutions/problems/median_of_two_sorted_arrays/solution.py
+++ solutions/problems/median_of_two_sorted_arrays/solution.py
@@ -1,30 +1,31 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
+        a = nums1
+        b = nums2
 
-        if len(B) < len(A):
-            A, B = B, A
-
-        l, r = 0, len(A) - 1
+        if len(b) < len(a):
+            a, b = b, a
+        
+        l, r = 0, len(a) - 1
 
         while True:
             midA = (r - l) // 2 + l
             midB = half - midA - 2
 
-            aLeft = A[midA] if midA >= 0 else float('-inf')
-            aRight = A[midA + 1] if midA + 1 < len(A) else float('inf')
-            bLeft = B[midB] if midB >= 0 else float('-inf')
-            bRight = B[midB + 1] if midB + 1 < len(B) else float('inf')
+            aLeft = a[midA] if midA >= 0 else float('-inf')
+            aRight = a[midA + 1] if midA + 1 < len(a) else float('inf')
+            bLeft = b[midB] if midB >= 0 else float('-inf')
+            bRight = b[midB + 1] if midB + 1 < len(b) else float('inf')
 
             if aLeft <= bRight and bLeft <= aRight:
                 if total % 2:
                     return min(aRight, bRight)
                 else:
                     return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
-            elif aLeft > bRight:
-                r = midA - 1
             else:
-                l = midA + 1
-        
+                if aLeft > bRight:
+                    r = midA - 1
+                else:
+                    l = midA + 1

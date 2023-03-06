dcf3e7c919be4bb4d9d4b6aa8d2369f07a858de6
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-06 18:55:10 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- solutions/problems/minimum_window_substring/solution.py
+++ solutions/problems/minimum_window_substring/solution.py
@@ -2,40 +2,36 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "": return ""
 
-        window, tCount = {}, {}
-        res, resLen = [-1, -1], float('inf')
+        tCounter, window = {}, {}
 
         for char in t:
-            tCount[char] = tCount.get(char, 0) + 1
+            tCounter[char] = tCounter.get(char, 0) + 1
+        
+        need, have = len(tCounter), 0
 
-        have, need = 0, len(tCount)
+        res = [-1, -1]
+        resLen = float('inf')
 
         l = 0
         for r in range(len(s)):
             char = s[r]
-            window[char] = window.get(char, 0) + 1
+            window[char] = window.get(char, 0) + 1 
 
-            if char in tCount and window[char] == tCount[char]:
+            if char in tCounter and tCounter[char] == window[char]:
                 have += 1
             
-            while need == have:
+            while have == need:
                 if (r - l + 1) < resLen:
                     res = [l, r]
                     resLen = r - l + 1
                 
                 window[s[l]] -= 1
-                if s[l] in tCount and window[s[l]] < tCount[s[l]]:
+                if s[l] in tCounter and window[s[l]] < tCounter[s[l]]:
                     have -= 1
-
                 l += 1
 
         l, r = res
-        return s[l : r + 1] if resLen != float('inf') else "" 
+        return s[l : r + 1] if resLen != float('inf') else ""
 
-
-
-        
-
-        
 
         

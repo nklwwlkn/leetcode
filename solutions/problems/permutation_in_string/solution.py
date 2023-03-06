532b532fc4d131b006124ce687efa61f1c41ab71
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-06 18:02:54 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- solutions/problems/permutation_in_string/solution.py
+++ solutions/problems/permutation_in_string/solution.py
@@ -3,20 +3,21 @@ class Solution:
         if len(s2) < len(s1): return False
 
         s1Counter, s2Counter = [0] * 26, [0] * 26
-        matches = 0
 
+        
         for idx in range(len(s1)):
             s1Counter[ord(s1[idx]) - ord('a')] += 1
             s2Counter[ord(s2[idx]) - ord('a')] += 1
-        
+
+        matches = 0
         for idx in range(26):
             matches += (1 if s1Counter[idx] == s2Counter[idx] else 0)
 
         l = 0
         for r in range(len(s1), len(s2)):
             if matches == 26: return True
-            
-            index = ord(s2[r]) - ord('a') 
+
+            index = ord(s2[r]) - ord('a')
             s2Counter[index] += 1
             if s1Counter[index] == s2Counter[index]:
                 matches += 1
@@ -28,8 +29,7 @@ class Solution:
             if s1Counter[index] == s2Counter[index]:
                 matches += 1
             elif s1Counter[index] - 1 == s2Counter[index]:
-                matches -= 1
-            
+                matches -= 1        
             l += 1
-        
+
         return matches == 26

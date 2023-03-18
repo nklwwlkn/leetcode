53cadeced2ab9d8b02646e44d276237c09482c84
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-18 11:59:44 +0000

    Sync LeetCode submission - Deepest Leaves Sum (python3)

---
+++ nklwwlkn/problems/deepest_leaves_sum/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        level = 0
+        d = defaultdict(list)
+
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    d[level].append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            
+            level += 1
+        
+        return sum(list(d.values())[-1])

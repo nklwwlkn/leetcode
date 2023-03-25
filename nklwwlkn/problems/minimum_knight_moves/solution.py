d4297c30c00a2e6becc96fd10e4bd06807d54bbe
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-25 12:59:30 +0000

    Sync LeetCode submission - Minimum Knight Moves (python3)

--- nklwwlkn/problems/minimum_knight_moves/solution.py
+++ nklwwlkn/problems/minimum_knight_moves/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        offsets = [(2, -1), (2, 1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-       
-        visited = set()
         q = deque([(0,0)])
         steps = 0
+        visited = set()
+
+        offsets = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
+
         while q:
             for _ in range(len(q)):
                 current_x, current_y = q.popleft()
@@ -13,9 +14,12 @@ class Solution:
                     return steps
 
                 for offset_x, offset_y in offsets:
-                    next_x, next_y = offset_x + current_x, offset_y + current_y
+                    next_x, next_y = current_x + offset_x, current_y + offset_y
+
                     if (next_x, next_y) not in visited:
-                        visited.add((next_x, next_y))
                         q.append((next_x, next_y))
+                        visited.add((next_x, next_y))            
             steps += 1
+
+        return -1
         

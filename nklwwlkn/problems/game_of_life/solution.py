2244d3675a1ebbe185ce77aa610cc6ac3dacc7cd
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-26 13:57:50 +0000

    Sync LeetCode submission - Game of Life (python3)

--- nklwwlkn/problems/game_of_life/solution.py
+++ nklwwlkn/problems/game_of_life/solution.py
@@ -3,33 +3,29 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+
         ROWS = len(board)
         COLS = len(board[0])
 
-        offsets = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        offsets = [(0, 1), (0, -1), (1, 0), (-1, 0), (1,1), (1, -1), (-1, 1), (-1,-1)]
+        
         for r in range(ROWS):
             for c in range(COLS):
-
-                # Count life neighbors:
                 live_neighbors = 0
-                for offset_x, offset_y in offsets:
-                    row = r + offset_x
-                    col = c + offset_y
+                for offset_r, offset_c in offsets:
+                    row = r + offset_r
+                    col = c + offset_c
 
                     if (row >= 0 and row < ROWS) and (col >= 0 and col < COLS) and abs(board[row][col]) == 1:
                         live_neighbors += 1
+            
 
-                # Rule 1 and Rule 3:
                 if board[r][c] == 1 and (live_neighbors < 2 or live_neighbors > 3):
-                    # -1 the cell is now dead, but originally was live:
                     board[r][c] = -1
-                
-                # Rule 4:
                 if board[r][c] == 0 and live_neighbors == 3:
-                    # 2 is now life, but originally was dead:
                     board[r][c] = 2
 
-        # Build original representation of the board:
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] > 0:

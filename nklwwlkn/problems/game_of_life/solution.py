c8961f1bcc425f82024abe6d0d6d37a3e80659e8
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-28 08:42:04 +0000

    Sync LeetCode submission - Game of Life (python3)

--- nklwwlkn/problems/game_of_life/solution.py
+++ nklwwlkn/problems/game_of_life/solution.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-
         ROWS = len(board)
         COLS = len(board[0])
 
+        offsets = [(1,0), (0,1), (-1, 0), (0, -1), (1, 1), (-1,-1), (1,-1), (-1,1)]
 
-        offsets = [(0, 1), (0, -1), (1, 0), (-1, 0), (1,1), (1, -1), (-1, 1), (-1,-1)]
-        
         for r in range(ROWS):
             for c in range(COLS):
                 live_neighbors = 0
@@ -19,16 +17,18 @@ class Solution:
 
                     if (row >= 0 and row < ROWS) and (col >= 0 and col < COLS) and abs(board[row][col]) == 1:
                         live_neighbors += 1
-            
-
+                
                 if board[r][c] == 1 and (live_neighbors < 2 or live_neighbors > 3):
                     board[r][c] = -1
                 if board[r][c] == 0 and live_neighbors == 3:
                     board[r][c] = 2
 
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] > 0:
                     board[r][c] = 1
                 else:
                     board[r][c] = 0
+
+        

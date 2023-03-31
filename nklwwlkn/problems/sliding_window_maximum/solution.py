135009026a2bb28a75ee01f8249e99807d6cf398
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-03-31 13:15:08 +0000

    Sync LeetCode submission - Sliding Window Maximum (python3)

--- nklwwlkn/problems/sliding_window_maximum/solution.py
+++ nklwwlkn/problems/sliding_window_maximum/solution.py
@@ -5,15 +5,14 @@ class Solution:
 
         l = 0
         for r in range(len(nums)):
-            while q and nums[q[-1]] < nums[r]:
+            while q and nums[r] > nums[q[-1]]:
                 q.pop()
             q.append(r)
 
-
             if l > q[0]:
                 q.popleft()
-            
-            if r - l + 1 == k:
+
+            if (r - l + 1) == k:
                 res.append(nums[q[0]])
                 l += 1
         

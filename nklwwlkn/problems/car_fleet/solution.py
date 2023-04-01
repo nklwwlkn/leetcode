1db528dfc767465aab3200124bfe3b65301502c2
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-04-01 17:20:47 +0000

    Sync LeetCode submission - Car Fleet (python3)

--- nklwwlkn/problems/car_fleet/solution.py
+++ nklwwlkn/problems/car_fleet/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        stack = [(position[i], speed[i]) for i in range(len(position))]
-        fleets = 0
+        grouped = [(position[i], speed[i]) for i in range(len(position))]
+        fleetCounter = 0
         currTime = 0
+        
 
-        for pos, spd in sorted(stack, reverse=True):
+        for pos, spd in sorted(grouped, reverse=True):
             destTime = (target - pos) / spd
             if destTime > currTime:
-                fleets += 1
                 currTime = max(currTime, destTime)
-            
-        return fleets
+                fleetCounter += 1
+        
+        return fleetCounter
+
+        
+        

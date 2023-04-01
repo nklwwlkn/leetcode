a7fffc7f0eaf146afd560cb2270fe94d3f21de7b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-04-01 15:03:42 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- nklwwlkn/problems/evaluate_reverse_polish_notation/solution.py
+++ nklwwlkn/problems/evaluate_reverse_polish_notation/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-
         for token in tokens:
             if token == "+":
                 stack.append(stack.pop() + stack.pop())
@@ -15,6 +14,7 @@ class Solution:
                 stack.append(stack.pop() * stack.pop())
             else:
                 stack.append(int(token))
-
-        return stack[-1]
         
+        return stack[-1]
+
+                

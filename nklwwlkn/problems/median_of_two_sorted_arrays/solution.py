e3d805c6f8edebb961c950c825698b777abddb5f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-04-02 15:11:45 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (python3)

--- nklwwlkn/problems/median_of_two_sorted_arrays/solution.py
+++ nklwwlkn/problems/median_of_two_sorted_arrays/solution.py
@@ -1,23 +1,22 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a, b = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
-        a = nums1
-        b = nums2
 
         if len(b) < len(a):
             a, b = b, a
+
+        l, r = 0, len(a) - 1 
         
-        l, r = 0, len(a) - 1
-
         while True:
-            midA = (r - l) // 2 + l
-            midB = half - midA - 2
+            aMid = (r - l) // 2 + l
+            bMid = half - aMid - 2
 
-            aLeft = a[midA] if midA >= 0 else float('-inf')
-            aRight = a[midA + 1] if midA + 1 < len(a) else float('inf')
-            bLeft = b[midB] if midB >= 0 else float('-inf')
-            bRight = b[midB + 1] if midB + 1 < len(b) else float('inf')
+            aLeft = a[aMid] if aMid >= 0 else float('-inf')
+            aRight = a[aMid + 1] if aMid + 1 < len(a) else float('inf')
+            bLeft = b[bMid] if bMid >= 0 else float('-inf')
+            bRight = b[bMid + 1] if bMid + 1 < len(b) else float('inf') 
 
             if aLeft <= bRight and bLeft <= aRight:
                 if total % 2:
@@ -26,6 +25,9 @@ class Solution:
                     return (max(aLeft, bLeft) + min(aRight, bRight)) / 2
             else:
                 if aLeft > bRight:
-                    r = midA - 1
+                    r = aMid - 1
                 else:
-                    l = midA + 1
+                    l = aMid + 1
+
+
+        

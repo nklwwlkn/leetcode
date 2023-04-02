cf91065fafb52638309211f31f6563e043da57b8
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-04-02 13:03:33 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- nklwwlkn/problems/koko_eating_bananas/solution.py
+++ nklwwlkn/problems/koko_eating_bananas/solution.py
@@ -6,21 +6,16 @@ class Solution:
 
         while l <= r:
             m = (r - l) // 2 + l
+
             time = 0
             for p in piles:
                 time += math.ceil(p / m)
-                
+
             if time <= h:
                 r = m - 1
                 k = min(k, m)
             else:
                 l = m + 1
-
+        
         return k
-                
-
-                
-
-
-
         

7b14e6dc479bb2a5d417f7cba478c2ab4d7b068b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-04-05 14:03:41 +0000

    Sync LeetCode submission - Validate Binary Search Tree (python3)

--- nklwwlkn/problems/validate_binary_search_tree/solution.py
+++ nklwwlkn/problems/validate_binary_search_tree/solution.py
@@ -9,13 +9,12 @@ class Solution:
         def valid(node, left, right):
             if not node:
                 return True
-            if not (node.val < right and node.val > left):
+            if not (left < node.val < right):
                 return False
-
+            
             return valid(node.left, left, node.val) and valid(
                 node.right, node.val, right
             )
 
         return valid(root, float("-inf"), float("inf"))
-    
-
+        

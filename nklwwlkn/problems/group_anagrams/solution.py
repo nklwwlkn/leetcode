7bdb6978885274fdd62e07a46ae86b87836a242b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-08-27 11:56:17 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- nklwwlkn/problems/group_anagrams/solution.py
+++ nklwwlkn/problems/group_anagrams/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(list)
-
         for word in strs:
-            mask = [0] * 26
+            hashed = [0] * 26
             for char in word:
-                mask[ord(char) - ord('a')] += 1
-            d[tuple(mask)].append(word)
-        return d.values()
-
+                hashed[ord(char) - ord('a')] += 1
+            d[tuple(hashed)].append(word)
         
+        return d.values()
+            

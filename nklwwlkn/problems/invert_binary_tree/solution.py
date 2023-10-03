f4256ec13721551407ebec38811ea9db3c480913
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-03 12:53:48 +0000

    Sync LeetCode submission - Invert Binary Tree (python3)

--- nklwwlkn/problems/invert_binary_tree/solution.py
+++ nklwwlkn/problems/invert_binary_tree/solution.py
@@ -10,10 +10,9 @@ class Solution:
             return None
 
         root.left, root.right = root.right, root.left
-
+        
         self.invertTree(root.left)
         self.invertTree(root.right)
 
         return root
-
         

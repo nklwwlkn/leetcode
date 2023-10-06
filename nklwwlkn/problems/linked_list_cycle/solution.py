33a86d7b5f0c5faa2f76752e1c48e56bc9a3101a
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-06 10:46:32 +0000

    Sync LeetCode submission - Linked List Cycle (python3)

--- nklwwlkn/problems/linked_list_cycle/solution.py
+++ nklwwlkn/problems/linked_list_cycle/solution.py
@@ -6,7 +6,9 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow, fast = head,head
+        slow = head
+        fast = head
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next

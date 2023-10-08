594d4fff61042ca31e134318d2b8379b4d743a94
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-08 19:59:21 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- nklwwlkn/problems/valid_anagram/solution.py
+++ nklwwlkn/problems/valid_anagram/solution.py
@@ -3,18 +3,16 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        counterT = dict()
         counterS = dict()
+        counterT = dict()
 
         for i in range(len(s)):
             counterS[s[i]] = counterS.get(s[i], 0) + 1
             counterT[t[i]] = counterT.get(t[i], 0) + 1
         
-
-        for key, val in counterT.items():
-            if key not in counterS or counterS[key] != val:
+        for key, value in counterS.items():
+            if not key in counterT or value != counterT.get(key):
                 return False
 
         return True
-
         

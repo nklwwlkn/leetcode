b097b9a82f0e31390446fab4ccf91e6c161a3698
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-10 20:40:23 +0000

    Sync LeetCode submission - Balanced Binary Tree (python3)

--- nklwwlkn/problems/balanced_binary_tree/solution.py
+++ nklwwlkn/problems/balanced_binary_tree/solution.py
@@ -9,10 +9,14 @@ class Solution:
         def dfs(root):
             if not root:
                 return [True, 0]
-            
-            left, right = dfs(root.left), dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
 
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+                
             return [balanced, max(left[1], right[1]) + 1]
 
+
         return dfs(root)[0]
+        

9f66b491655a8b1759a6524684dd5536d53abd02
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-10 21:02:09 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- nklwwlkn/problems/climbing_stairs/solution.py
+++ nklwwlkn/problems/climbing_stairs/solution.py
@@ -1,8 +1,10 @@
 class Solution:
-    hashmap = { 0 : 0, 1 : 1, 2 : 2 }
+    memo = { 1 : 1, 2 : 2 }
+
     def climbStairs(self, n: int) -> int:
-        if n in self.hashmap:
-            return self.hashmap[n]
+        if n in self.memo:
+            return self.memo.get(n)
         else:
-            self.hashmap[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-            return self.hashmap[n]
+            self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            return self.memo.get(n)
+        

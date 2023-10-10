8ed8a7a8fe8b6c924c77d60f8f044d6c1f6a32ba
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-10 20:34:58 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (python3)

--- nklwwlkn/problems/maximum_depth_of_binary_tree/solution.py
+++ nklwwlkn/problems/maximum_depth_of_binary_tree/solution.py
@@ -9,4 +9,8 @@ class Solution:
         if not root:
             return 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
+
+
+        

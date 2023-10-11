8748f7ea5dd15b69a48fc59f796962d3864b64c2
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-11 15:40:53 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- nklwwlkn/problems/contains_duplicate/solution.py
+++ nklwwlkn/problems/contains_duplicate/solution.py
@@ -1,10 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        d = dict()
-
-        for num in nums:
-            d[num] = d.get(num, 0) + 1
-            if d.get(num) >= 2:
-                return True
-
-        return False
+        return len(set(nums)) != len(nums)

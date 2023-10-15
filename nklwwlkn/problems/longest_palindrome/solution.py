99d59e761e190279a1cd7fd664a24d2cd2630026
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-15 14:13:01 +0000

    Sync LeetCode submission - Longest Palindrome (python3)

--- nklwwlkn/problems/longest_palindrome/solution.py
+++ nklwwlkn/problems/longest_palindrome/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hm = dict()
+        charCounter = Counter(s)
+
         length = 0
         hasOdd = False
 
-        for char in s:
-            hm[char] = hm.get(char, 0) + 1
-
-        for count in hm.values():
-            if count % 2 == 0:
-                length += count
+        for key, value in charCounter.items():
+            if value % 2 == 0:
+                length += value
             else:
-                length += count - 1
+                length += value - 1
                 hasOdd = True
-        
-        return length + 1 if hasOdd else length
-
             
         
+        return length + 1 if hasOdd else length

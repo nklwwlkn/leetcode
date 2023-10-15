e58b5c288a644f9e58f38038b79f1d8f67e86cf6
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-15 14:23:13 +0000

    Sync LeetCode submission - Majority Element (python3)

--- nklwwlkn/problems/majority_element/solution.py
+++ nklwwlkn/problems/majority_element/solution.py
@@ -1,17 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        res, count = 0, 0
+        count = 0
+        res = None
 
         for num in nums:
-            if count == 0:
+            if count <= 0:
                 res = num
             
-            if num == res:
-                count += 1
-            else:
-                count -= 1
-
+            count = count + 1 if num == res else count - 1
+        
         return res
-            
-
         

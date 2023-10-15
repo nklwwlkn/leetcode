1c7a39a927a605687a90a30a12f25ec7f24082ae
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-15 12:45:42 +0000

    Sync LeetCode submission - Balanced Binary Tree (python3)

--- nklwwlkn/problems/balanced_binary_tree/solution.py
+++ nklwwlkn/problems/balanced_binary_tree/solution.py
@@ -14,7 +14,7 @@ class Solution:
             right = dfs(root.right)
 
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-                
+        
             return [balanced, max(left[1], right[1]) + 1]
 
 

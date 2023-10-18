9eed54e6b0a693fbba6dad1db20f241ac2f85e49
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-18 13:26:32 +0000

    Sync LeetCode submission - Backspace String Compare (python3)

--- nklwwlkn/problems/backspace_string_compare/solution.py
+++ nklwwlkn/problems/backspace_string_compare/solution.py
@@ -1,14 +1,45 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def build(word):
-            stack = []
-            for char in word:
-                if char != "#":
-                    stack.append(char)
-                elif stack:
-                    stack.pop()
-            
-            return "".join(stack)
+        sPointer = len(s) - 1
+        tPointer = len(t) - 1
 
-        return build(s) == build(t)
+        sSkips = 0
+        tSkips = 0
+
+        while sPointer >= 0 or tPointer >= 0:
+            while sPointer >= 0:
+                if s[sPointer] == "#":
+                    sSkips += 1
+                    sPointer -= 1
+                elif sSkips > 0:
+                    sSkips -= 1
+                    sPointer -= 1
+                else:
+                    break
+
+            while tPointer >= 0:
+                if t[tPointer] == "#":
+                    tSkips += 1
+                    tPointer -= 1
+                elif tSkips > 0:
+                    tSkips -= 1
+                    tPointer -= 1
+                else:
+                    break
+
+            if sPointer >= 0 and tPointer >= 0 and s[sPointer] != t[tPointer]:
+                return False
+
+            if (sPointer >= 0) != (tPointer >= 0):
+                return False
+
+            if sPointer >= 0:
+                sPointer -= 1
+            if tPointer >= 0:
+                tPointer -= 1
         
+        return True
+
+            
+
+            

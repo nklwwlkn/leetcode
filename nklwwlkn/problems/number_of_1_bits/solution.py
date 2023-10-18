49e61538bdce9a12b43d185d94af94df08c988b2
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-18 19:22:20 +0000

    Sync LeetCode submission - Number of 1 Bits (python3)

---
+++ nklwwlkn/problems/number_of_1_bits/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        counter = 0
+
+        while n:
+            n = n & (n - 1)
+            counter += 1
+        
+        return counter
+        

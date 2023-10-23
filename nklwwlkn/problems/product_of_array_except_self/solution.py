bada357864ed5633efc02de4b96b41ae1e95044b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-23 09:51:19 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- nklwwlkn/problems/product_of_array_except_self/solution.py
+++ nklwwlkn/problems/product_of_array_except_self/solution.py
@@ -8,10 +8,9 @@ class Solution:
             prefix *= nums[i]
 
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
 
         return res
-
         

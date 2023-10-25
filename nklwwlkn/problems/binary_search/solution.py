1f89e6be98e81d79015dfa044af50032f9e2ce2f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-25 13:32:21 +0000

    Sync LeetCode submission - Binary Search (python3)

--- nklwwlkn/problems/binary_search/solution.py
+++ nklwwlkn/problems/binary_search/solution.py
@@ -5,14 +5,14 @@ class Solution:
         while l <= r:
             m = l + (r - l) // 2
 
-            if target == nums[m]:
+            if nums[m] == target:
                 return m
-
+            
             if target > nums[m]:
                 l = m + 1
             else:
                 r = m - 1
-            
+        
 
         return -1
         

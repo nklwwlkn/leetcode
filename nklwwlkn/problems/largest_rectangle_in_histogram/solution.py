114af03f1a0552521fca5195f8192389f3e49ee1
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-10-28 12:53:38 +0000

    Sync LeetCode submission - Largest Rectangle in Histogram (python3)

--- nklwwlkn/problems/largest_rectangle_in_histogram/solution.py
+++ nklwwlkn/problems/largest_rectangle_in_histogram/solution.py
@@ -3,20 +3,16 @@ class Solution:
         stack = []
         largest = 0
 
-        heights.append(0)
-
-
         for idx, height in enumerate(heights):
             start = idx
-            while stack and stack[-1][1] >= height:
+            while stack and stack[-1][1] > height:
                 iStack, hStack = stack.pop()
-                largest = max(largest, hStack * (idx  - iStack))
+                largest = max(largest, hStack * (idx - iStack))
                 start = iStack
             stack.append([start, height])
         
-        print(stack)
-
-
+        for i, h in stack:
+            largest = max(largest, h * (len(heights) - i))
 
         return largest
         

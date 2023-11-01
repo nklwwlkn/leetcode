2a4dd59e0b3dd10382a3208ad82a0509215169c5
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-01 16:08:38 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- nklwwlkn/problems/permutation_in_string/solution.py
+++ nklwwlkn/problems/permutation_in_string/solution.py
@@ -5,17 +5,18 @@ class Solution:
         s1Counter = [0] * 26
         s2Counter = [0] * 26
 
-        for idx in range(len(s1)):
-            s1Counter[ord(s1[idx]) - ord('a')] += 1
-            s2Counter[ord(s2[idx]) - ord('a')] += 1
-        
+        for i in range(len(s1)):
+            s1Counter[ord(s1[i]) - ord('a')] += 1
+            s2Counter[ord(s2[i]) - ord('a')] += 1
+
         matches = 0
-        for idx in range(26):
-            matches += (1 if s1Counter[idx] == s2Counter[idx] else 0)
+        for i in range(26):
+            matches += 1 if s1Counter[i] == s2Counter[i] else 0
 
         l = 0
         for r in range(len(s1), len(s2)):
-            if matches == 26: return True
+            if matches == 26:
+                return True
 
             index = ord(s2[r]) - ord('a')
             s2Counter[index] += 1
@@ -23,14 +24,14 @@ class Solution:
                 matches += 1
             elif s1Counter[index] + 1 == s2Counter[index]:
                 matches -= 1
-
+            
             index = ord(s2[l]) - ord('a')
             s2Counter[index] -= 1
             if s1Counter[index] == s2Counter[index]:
                 matches += 1
             elif s1Counter[index] - 1 == s2Counter[index]:
                 matches -= 1
-        
+            
             l += 1
-
+        
         return matches == 26

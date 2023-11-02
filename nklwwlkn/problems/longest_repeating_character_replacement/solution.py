77f66aa4e3a3eabf4ceb7dc2e9bedf7e8c81bdd8
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-02 12:27:20 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- nklwwlkn/problems/longest_repeating_character_replacement/solution.py
+++ nklwwlkn/problems/longest_repeating_character_replacement/solution.py
@@ -6,10 +6,12 @@ class Solution:
         l = 0
         for r in range(len(s)):
             hm[s[r]] = hm.get(s[r], 0) + 1
-
-            while ((r - l + 1) - max(hm.values()) > k):
+            
+            while ((r - l + 1) - max(hm.values())) > k:
                 hm[s[l]] -= 1
                 l += 1
             longest = max(longest, r - l + 1)
+        
+        return longest
 
-        return longest        
+        

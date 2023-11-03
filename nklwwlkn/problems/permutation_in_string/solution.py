25b07241ee8e7f85d48d4651fc0d1b9d563b8c6e
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-03 10:32:04 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- nklwwlkn/problems/permutation_in_string/solution.py
+++ nklwwlkn/problems/permutation_in_string/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1): return False
+        if len(s2) < len(s1):
+            return False
 
         s1Counter = [0] * 26
         s2Counter = [0] * 26
 
         for i in range(len(s1)):
             s1Counter[ord(s1[i]) - ord('a')] += 1
-            s2Counter[ord(s2[i]) - ord('a')] += 1
+            s2Counter[ord(s2[i]) - ord('a')] += 1 
 
         matches = 0
         for i in range(26):
@@ -22,16 +23,18 @@ class Solution:
             s2Counter[index] += 1
             if s1Counter[index] == s2Counter[index]:
                 matches += 1
-            elif s1Counter[index] + 1 == s2Counter[index]:
+            elif s1Counter[index] == s2Counter[index] - 1:
                 matches -= 1
             
             index = ord(s2[l]) - ord('a')
             s2Counter[index] -= 1
             if s1Counter[index] == s2Counter[index]:
                 matches += 1
-            elif s1Counter[index] - 1 == s2Counter[index]:
+            elif s1Counter[index] == s2Counter[index] + 1:
                 matches -= 1
             
             l += 1
-        
+
         return matches == 26
+        
+        

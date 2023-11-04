31bb33fff593841aad66748580e2f490ff8e79dd
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-04 00:08:19 +0000

    Sync LeetCode submission - Diameter of Binary Tree (python3)

--- nklwwlkn/problems/diameter_of_binary_tree/solution.py
+++ nklwwlkn/problems/diameter_of_binary_tree/solution.py
@@ -7,19 +7,17 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = [0]
-
         def dfs(root):
             if not root:
                 return 0
-
+            
             left = dfs(root.left)
             right = dfs(root.right)
 
             res[0] = max(res[0], left + right)
 
             return max(left, right) + 1
-
+        
         dfs(root)
 
         return res[0]
-        

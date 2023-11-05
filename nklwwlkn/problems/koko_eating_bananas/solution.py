fbc90a0875d94471c702a512559854c8bb40bfe2
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-05 20:14:04 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- nklwwlkn/problems/koko_eating_bananas/solution.py
+++ nklwwlkn/problems/koko_eating_bananas/solution.py
@@ -2,22 +2,21 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         maxPiles = max(piles)
 
-        l, r = 1, maxPiles
-
         speed = maxPiles
 
+        l, r = 1, maxPiles
+        
         while l <= r:
             m = l + (r - l) // 2
-
+            
             time = 0
             for p in piles:
                 time += math.ceil(p / m)
-            
+
             if time <= h:
-                speed = min(speed, m)
                 r = m - 1
+                speed = min(speed, m)
             else:
                 l = m + 1
-
-
-        return speed
+        
+        return speed 

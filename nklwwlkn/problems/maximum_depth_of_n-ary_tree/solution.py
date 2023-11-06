e4f3a0f37ccb5ca07f5439f8933befdc45795926
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-06 16:03:41 +0000

    Sync LeetCode submission - Maximum Depth of N-ary Tree (python3)

---
+++ nklwwlkn/problems/maximum_depth_of_n-ary_tree/solution.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+
+        q = deque([root])
+
+        level = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                
+                if node:
+                    for child in node.children:
+                        q.append(child)
+            level += 1
+
+
+        return level
+        

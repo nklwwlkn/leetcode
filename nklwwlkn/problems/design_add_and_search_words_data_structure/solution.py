3e98144ec2fb2c2e711f4e87e54d34638e495995
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-11 20:49:20 +0000

    Sync LeetCode submission - Design Add and Search Words Data Structure (python3)

--- nklwwlkn/problems/design_add_and_search_words_data_structure/solution.py
+++ nklwwlkn/problems/design_add_and_search_words_data_structure/solution.py
@@ -1,22 +1,27 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.isEnd = False
 
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
+        self.maxWordLen = 0
 
     def addWord(self, word: str) -> None:
         curr = self.root
-
         for char in word:
             if char not in curr.children:
                 curr.children[char] = TrieNode()
             curr = curr.children[char]
+
         curr.isEnd = True
+        self.maxWordLen = max(self.maxWordLen, len(word))
 
     def search(self, word: str) -> bool:
+        if len(word) > self.maxWordLen: return False
+    
         def dfs(j, root):
             curr = root
             for i in range(j, len(word)):
@@ -30,11 +35,12 @@ class WordDictionary:
                     if char not in curr.children:
                         return False
                     curr = curr.children[char]
-            
+
             return curr.isEnd
+        
         return dfs(0, self.root)
 
-        
+
 
 
 # Your WordDictionary object will be instantiated and called as such:

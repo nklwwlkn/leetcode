6c4cf3f81569d400afdd771d70203b63571614c3
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-16 13:27:26 +0000

    Sync LeetCode submission - Word Search (python3)

--- nklwwlkn/problems/word_search/solution.py
+++ nklwwlkn/problems/word_search/solution.py
@@ -7,39 +7,32 @@ class Solution:
 
         letterCounter = Counter(word)
 
-        if letterCounter[word[-1]] < letterCounter[word[0]]:
+        if letterCounter[word[0]] > letterCounter[word[-1]]:
             word = word[::-1]
 
-        def dfs(r, c, letterIndex):
-            if letterIndex == len(word):
+        def dfs(r, c, wordIndex):
+            if wordIndex >= len(word):
                 return True
 
-            if (
-                r < 0
-                or r >= ROWS
-                or c < 0
-                or c >= COLS
-                or (r, c) in visited
-                or board[r][c] != word[letterIndex]
-            ):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or (r, c) in visited or board[r][c] != word[wordIndex]:
                 return False
 
-            visited.add((r, c))
+            visited.add((r,c))
 
             res = (
-                dfs(r + 1, c, letterIndex + 1)
-                or dfs(r - 1, c, letterIndex + 1)
-                or dfs(r, c + 1, letterIndex + 1)
-                or dfs(r, c - 1, letterIndex + 1)
+                dfs(r + 1, c, wordIndex + 1)
+                or dfs(r, c + 1, wordIndex + 1)
+                or dfs(r - 1, c, wordIndex + 1)
+                or dfs(r, c - 1, wordIndex + 1)
             )
 
-            visited.remove((r, c))
+            visited.remove((r,c))
 
             return res
-
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0):
                     return True
-
+    
         return False

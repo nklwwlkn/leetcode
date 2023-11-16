a3899d069699c093472b3e0c890b589016284f3e
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-16 16:28:36 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- nklwwlkn/problems/palindrome_partitioning/solution.py
+++ nklwwlkn/problems/palindrome_partitioning/solution.py
@@ -1,29 +1,35 @@
 class Solution:
-    def isPalindrome(self, s, l, r):
+    def isPalindrome(self, l, r, s):
         while l < r:
             if s[l] != s[r]:
                 return False
-            
+
             l += 1
             r -= 1
-        
+
         return True
 
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        substring = []
+        partition = []
 
         def backtrack(j):
-            if j >= len(s): 
-                res.append(substring.copy())
+            if j >= len(s):
+                res.append(partition.copy())
                 return
 
-            for i in range(j ,len(s)):
-                if self.isPalindrome(s, j, i):
-                    substring.append(s[j : i + 1])
-                    backtrack(i + 1)
-                    substring.pop()
+            for i in range(j, len(s)):
+                if self.isPalindrome(j, i, s):
+                    partition.append(s[j : i + 1])
 
+                    backtrack(i + 1)
+
+                    partition.pop()
+            
+        
         backtrack(0)
 
         return res
+
+
+        

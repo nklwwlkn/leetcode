7ccbe36449339a7e02443b12247d30224e99c0c6
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-17 12:12:30 +0000

    Sync LeetCode submission - Sudoku Solver (python3)

--- nklwwlkn/problems/sudoku_solver/solution.py
+++ nklwwlkn/problems/sudoku_solver/solution.py
@@ -1,21 +1,20 @@
-from collections import defaultdict
-
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
+
         takenRows = defaultdict(set)
         takenCols = defaultdict(set)
         takenSqrs = defaultdict(set)
+        
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] != ".":
-                    num = board[r][c]
-                    takenRows[r].add(num)
-                    takenCols[c].add(num)
-                    takenSqrs[(r // 3, c // 3)].add(num)
+                    takenRows[r].add(board[r][c])
+                    takenCols[c].add(board[r][c])
+                    takenSqrs[(r // 3, c // 3)].add(board[r][c])
 
         def backtrack(r, c):
             if c == 9:
@@ -24,27 +23,28 @@ class Solution:
 
             if r == 9:
                 return True
-
+            
             if board[r][c] != ".":
                 return backtrack(r, c + 1)
 
             for i in range(1, 10):
                 num = str(i)
-                if num not in takenRows[r] and num not in takenCols[c] and num not in takenSqrs[(r // 3, c // 3)]:
-                   
-                    board[r][c] = num
-                    takenRows[r].add(num)
-                    takenCols[c].add(num)
-                    takenSqrs[(r // 3, c // 3)].add(num)
 
-                    if backtrack(r, c + 1):
-                        return True
+                if num in takenRows[r] or num in takenCols[c] or num in takenSqrs[(r // 3, c // 3)]: continue
 
-                    board[r][c] = "."
-                    takenRows[r].remove(num)
-                    takenCols[c].remove(num)
-                    takenSqrs[(r // 3, c // 3)].remove(num)
+                board[r][c] = num
+                takenRows[r].add(num)
+                takenCols[c].add(num)
+                takenSqrs[(r // 3, c // 3)].add(num)
 
+                if backtrack(r, c + 1):
+                    return True
+                
+                board[r][c] = "."
+                takenRows[r].remove(num)
+                takenCols[c].remove(num)
+                takenSqrs[(r // 3, c // 3)].remove(num)
+            
             return False
-
+        
         backtrack(0, 0)

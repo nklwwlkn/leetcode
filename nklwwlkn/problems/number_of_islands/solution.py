69b416ef310758820232b29ab18d1e3d9112c09d
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-11-23 10:53:55 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- nklwwlkn/problems/number_of_islands/solution.py
+++ nklwwlkn/problems/number_of_islands/solution.py
@@ -1,27 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        counter = 0
+        rows, cols = len(grid), len(grid[0])
         visited = set()
-        rows = len(grid)
-        cols = len(grid[0])
+        islands = 0
 
-        def dfs(r, c):
-            if r < 0 or r == rows or c < 0 or c == cols or (r,c) in visited or grid[r][c] == "0":
-                return 0
+        def bfs(r, c):
+            queue = deque()
+            queue.append((r, c))
+            visited.add((r, c))
 
-            visited.add((r,c))
-
-            dfs(r + 1, c)
-            dfs(r, c + 1)
-            dfs(r - 1, c)
-            dfs(r, c - 1)
-
-            return 1
-
+            while queue:
+                row, col = queue.popleft()
+                directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and c in range(cols) and
+                            grid[r][c] == '1' and (r, c) not in visited):
+                        queue.append((r, c))
+                        visited.add((r, c))
 
         for r in range(rows):
             for c in range(cols):
-                counter += dfs(r, c)
-            
-        
-        return counter
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+
+        return islands

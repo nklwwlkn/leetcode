555e41506dec0e755268979167c49d9ccceb532f
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-04 15:26:17 +0000

    Sync LeetCode submission - Max Area of Island (python3)

--- nklwwlkn/problems/max_area_of_island/solution.py
+++ nklwwlkn/problems/max_area_of_island/solution.py
@@ -5,7 +5,7 @@ class Solution:
         visited = set()
 
         def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or grid[r][c] == 0:
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0 or (r, c) in visited:
                 return 0
 
             visited.add((r, c))
@@ -18,13 +18,11 @@ class Solution:
             return top + right + down + left + 1
 
 
-
-
         res = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) not in visited and grid[r][c] == 1:
-                    res = max(res, dfs(r,c))
-        
+                if grid[r][c] == 1 and not (r, c) in visited:
+                    res = max(res, dfs(r, c))
+
+
         return res
-        

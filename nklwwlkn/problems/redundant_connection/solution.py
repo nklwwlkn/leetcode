4b8e857efbf14ea14a32868e9367e6890913e366
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-05 12:05:08 +0000

    Sync LeetCode submission - Redundant Connection (python3)

--- nklwwlkn/problems/redundant_connection/solution.py
+++ nklwwlkn/problems/redundant_connection/solution.py
@@ -2,14 +2,17 @@ class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
         self.rank = [1] * size
-    
+
     def find(self, n):
         if n == self.root[n]:
             return n
-
+        
         self.root[n] = self.find(self.root[n])
 
         return self.root[n]
+
+    def isConnected(self, n1, n2):
+        return self.find(n1) == self.find(n2)
 
     def union(self, n1, n2):
         root1 = self.find(n1)
@@ -18,21 +21,22 @@ class UnionFind:
         if root1 != root2:
             if self.rank[root1] < self.rank[root2]:
                 self.root[root1] = root2
-            elif self.rank[root1] > self.rank[root2]:
+            elif self.rank[root2] < self.rank[root1]:
                 self.root[root2] = root1
             else:
                 self.root[root1] = root2
-                self.rank[root1] += 1
+                self.rank[root1] += 1 
+            
             return True
-        else:
-            return False 
         
-        
+        return False
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
         uf = UnionFind(len(edges) + 1)
 
         for u, v in edges:
             if not uf.union(u, v):
                 return [u, v]
+        

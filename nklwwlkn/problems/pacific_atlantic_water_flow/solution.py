4411395c64cde81db6e5d77911d9050ad6d4436a
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-05 14:40:14 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (python3)

--- nklwwlkn/problems/pacific_atlantic_water_flow/solution.py
+++ nklwwlkn/problems/pacific_atlantic_water_flow/solution.py
@@ -5,26 +5,30 @@ class Solution:
 
         pac = set()
         atl = set()
-
-        def dfs(r, c, visited, prev):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or prev > heights[r][c]:
-                return 
+        
+        def dfs(r, c, visited, prev = None):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or (prev and heights[r][c] < prev):
+                return
 
             visited.add((r, c))
 
             dfs(r + 1, c, visited, heights[r][c])
-            dfs(r, c + 1, visited, heights[r][c])
             dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
 
+        
+        for r in range(ROWS):
+            dfs(r, COLS - 1, atl)
+            dfs(r, 0, pac)
 
         for c in range(COLS):
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-            dfs(0, c, pac, heights[0][c])
-
-        for r in range(ROWS):
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
-            dfs(r, 0, pac, heights[r][0])
+            dfs(ROWS - 1, c, atl)
+            dfs(0, c, pac)
 
         return pac.intersection(atl)
+
         
+
+
+            

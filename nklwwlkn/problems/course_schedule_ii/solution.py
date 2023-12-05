e15c1e0c2cb61f3bbfe2ae33b93c41e0f8675742
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-05 11:25:52 +0000

    Sync LeetCode submission - Course Schedule II (python3)

--- nklwwlkn/problems/course_schedule_ii/solution.py
+++ nklwwlkn/problems/course_schedule_ii/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        topological = []
+        visited = set()
+        cycle = set()
+
         adjList = defaultdict(list)
-        visiting = set()
-        visited  = set()
-        res = []
 
         for course, prereq in prerequisites:
             adjList[course].append(prereq)
@@ -11,25 +12,27 @@ class Solution:
         
         def dfs(course):
             if course in visited:
-                return True
-            
-            if course in visiting:
-                return False
-            
-            visiting.add(course)
+                return True 
 
+            if course in cycle:
+                return False
+
+            cycle.add(course)
             for prereq in adjList[course]:
                 if not dfs(prereq):
                     return False
-            
+
             visited.add(course)
-            res.append(course)
-        
+            topological.append(course)
+
             return True
 
         
         for course in range(numCourses):
             if not dfs(course):
                 return []
+
+        return topological
+
+
         
-        return res

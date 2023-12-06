aff546270ea82c2b129fa2b7aac4e82018f9da38
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-06 13:05:17 +0000

    Sync LeetCode submission - Cheapest Flights Within K Stops (python3)

--- nklwwlkn/problems/cheapest_flights_within_k_stops/solution.py
+++ nklwwlkn/problems/cheapest_flights_within_k_stops/solution.py
@@ -1,28 +1,27 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         adjList = defaultdict(list)
-        costs = []
 
         for fr, to, cost in flights:
             adjList[fr].append((cost, to))
-
+        
         minHeap = [(0, src, 0)]
-        minCost = {}
+        nodeToStops= {}
 
         while minHeap:
             cCost, cNode, cStops = heapq.heappop(minHeap)
 
-            if cNode in minCost and minCost[cNode] <= cStops:
+            if cNode in nodeToStops and cStops >= nodeToStops[cNode]:
                 continue
 
-            minCost[cNode] = cStops
+            nodeToStops[cNode] = cStops
 
             if cNode == dst:
                 return cCost
-            
+
             if cStops <= k:
                 for nCost, nNode in adjList[cNode]:
                     heapq.heappush(minHeap, (cCost + nCost, nNode, cStops + 1))
+            
 
         return -1
-        

a76d4e761b1631d0dcab29647715c322b3f868ce
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-06 12:42:25 +0000

    Sync LeetCode submission - Network Delay Time (python3)

--- nklwwlkn/problems/network_delay_time/solution.py
+++ nklwwlkn/problems/network_delay_time/solution.py
@@ -2,30 +2,31 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adjList = defaultdict(list)
 
-        for source, target, cost in times:
-            adjList[source].append((cost, target))
-        
-        nodeToMin = {node : float('inf') for node in range(1, n + 1)}
+        for time in times:
+            time[0], time[2] = time[2], time[0]
+
+        for w, v, u in times:
+            adjList[u].append((w, v))
 
         minHeap = [(0, k)]
+        nodeToMin = {node : float('inf') for node in range(1, n + 1)}
         nodeToMin[k] = 0
 
         while minHeap:
-            currentCost, currentNode = heapq.heappop(minHeap)
+            cWeight, cNode = heapq.heappop(minHeap)
 
-            if currentCost > nodeToMin[currentNode]:
-                continue
-            
-            for nCost, nNode in adjList[currentNode]:
-                cost = currentCost + nCost
+            if cWeight > nodeToMin[cNode]: continue 
+
+            for nWeight, nNode in adjList[cNode]:
+                cost = cWeight + nWeight 
 
                 if cost < nodeToMin[nNode]:
-                    nodeToMin[nNode] = cost
                     heapq.heappush(minHeap, (cost, nNode))
-        
-
+                    nodeToMin[nNode] = cost
 
         res = max(nodeToMin.values())
 
         return res if res != float('inf') else -1
-        
+
+
+                

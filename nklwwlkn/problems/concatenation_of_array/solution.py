599d3111b614bdc6ab4e5aa42400954a99b6c0cd
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-20 16:03:38 +0000

    Sync LeetCode submission - Concatenation of Array (python3)

--- nklwwlkn/problems/concatenation_of_array/solution.py
+++ nklwwlkn/problems/concatenation_of_array/solution.py
@@ -1,11 +1,16 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        ans = [0] * (size * 2)
+        ans = []
 
-        for i in range(len(nums)):
-            j = size + i
-            ans[i] = nums[i]
-            ans[j] = nums[i]
+        i = 0
+        while len(ans) < len(nums) * 2:
+            if i >= len(nums):
+                i = 0
+            ans.append(nums[i])
+            i += 1
         
         return ans
+
+
+
+        

a91fcdb6bebc767d3be7c7895b30645c7a4ab33b
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-25 14:25:07 +0000

    Sync LeetCode submission - Find Pivot Index (python3)

---
+++ nklwwlkn/problems/find_pivot_index/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        prefix = 0
+        for idx, num in enumerate(nums):
+            if prefix ==  (total - prefix - num):
+                return idx
+            
+            prefix += num
+        
+        return -1
+
+        

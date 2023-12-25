9ab2393dbf1ebe3620e1111c88b2610df182cd49
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2023-12-25 16:53:45 +0000

    Sync LeetCode submission - Word Pattern (python3)

---
+++ nklwwlkn/problems/word_pattern/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        splitted = s.split()
+        patternToS = dict()
+        sToPattern = dict()
+
+        if len(pattern) != len(splitted):
+            return False
+        
+        for idx, pChar in enumerate(pattern):
+            if (pChar not in patternToS) and (splitted[idx] not in sToPattern):
+                patternToS[pChar] = splitted[idx]
+                sToPattern[splitted[idx]] = pChar
+            elif pChar in patternToS and patternToS[pChar] != splitted[idx]:
+                return False
+            elif splitted[idx] in sToPattern and sToPattern[splitted[idx]] != pChar:
+                return False
+          
+        return True
+
+
+        

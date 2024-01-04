ec21873680afacd81b3038c9aac0668a7e40ba5a
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2024-01-04 13:01:12 +0000

    Sync LeetCode submission - Validate Stack Sequences (python3)

---
+++ nklwwlkn/problems/validate_stack_sequences/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        i = 0
+        for item in pushed:
+            stack.append(item)
+            while stack and popped and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+        
+        return len(stack) == 0
+        
